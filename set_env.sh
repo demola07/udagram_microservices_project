0347f5ed123e123e2ef050339733c186d010cc53
Ademola Sobaki <demolasobaki@gmail.com>
2022-06-16 16:18:28 +0100

    initial project commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,21 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+setx POSTGRES_USERNAME postgres
+setx POSTGRES_PASSWORD admin001
+setx POSTGRES_HOST udacity-project.cfdhq6tdakph.eu-west-1.rds.amazonaws.com
+setx POSTGRES_DB postgres
+setx AWS_BUCKET udacity-251192102041-dev
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET hello
+setx URL http://localhost:8100
+
+# export POSTGRES_USERNAME=postgres
+# export POSTGRES_PASSWORD=mypassword
+# export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=postgres
+# export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+# export AWS_REGION=us-east-1
+# export AWS_PROFILE=default
+# export JWT_SECRET=testing
+# export URL=http://localhost:8100
